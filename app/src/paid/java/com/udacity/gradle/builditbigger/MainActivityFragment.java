95904f64ca792adbf66090b9e5bcedb59935e816
ygarcia <garciayuni84@gmail.com>
2017-08-09 18:46:46 -0400

    error check manifest2

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,30 @@
+package com.udacity.gradle.builditbigger.free;
+package com.udacity.gradle.builditbigger.R;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    public MainActivityFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        return root;
+    }
+
+}
