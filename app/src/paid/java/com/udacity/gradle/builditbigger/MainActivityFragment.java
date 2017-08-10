dd0b8cc4a5a4119123dc980a7a9362ff52b12d3d
yggarcia <garciayuni84@gmail.com>
2017-08-09 23:50:08 -0400

    final project submission 1

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,5 +1,4 @@
-package com.udacity.gradle.builditbigger.free;
-package com.udacity.gradle.builditbigger.R;
+package com.udacity.gradle.builditbigger;
 
 
 import android.os.Bundle;
--- jokesproviderlib/src/main/java/com/example/JokeTeller.java
+++ jokesproviderlib/src/main/java/com/example/JokeTeller.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class JokeTeller {
     public String tellAJoke(){
-        return "jajajajajajajaajaja";
+        return "A soldier survived mustard gas in battle, and then pepper spray by the police. He's now a seasoned veteran.";
     }
 }
